36a96ba4cae5ceea6d61141c61925e4ea389cd8d
furrymcgee <furrymcgee@users.noreply.github.com>
2019-08-06 19:39:01 +0200

    rm

--- autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-/usr/sbin/install-docs -v -i /etc/doc-base/documents/foo
-: /usr/sbin/dwww-index++ -v -f -- -v4
-: /usr/bin/search --config-file /usr/share/dwww/swish++.conf --index-file /var/cache/dwww/dwww.swish++.index -D
-
