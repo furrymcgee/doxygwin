9d82f0d0b89e87ad46d05a7f2bc5473745957018
furrymcgee <furrymcgee@users.noreply.github.com>
2019-11-01 16:44:06 +0100

    debconf

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
+# This script builds https://github.com/furrymcgee/doxie-search
+# First submodules are patched then make is called
 
-git submodule update
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+git submodule update --init --force --checkout
 
 cat \
 0001-cygwin.patch.~1~ \
 0002-samba.patch \
-| GIT_WORK_TREE=sensible-utils GIT_DIR=sensible-utils/.git git am
+| \
+GIT_WORK_TREE=sensible-utils GIT_DIR=sensible-utils/.git git am
 
 cat \
 0001-index-on-no-branch-fa850df-Bump-version-to-0.25.0.patch \
-| GIT_WORK_TREE=cygport GIT_DIR=cygport/.git git am
+| \
+GIT_WORK_TREE=cygport GIT_DIR=cygport/.git git am
 
 cat \
 0001-index-on-no-branch-d7885fe-Fix-check-for-attempt-to-.patch \
-| GIT_WORK_TREE=calm GIT_DIR=calm/.git git am
+| \
+GIT_WORK_TREE=calm GIT_DIR=calm/.git git am
 
 cat \
 0001-fixup-Release-1.19.7.patch \
@@ -23,35 +31,45 @@ cat \
 0005-fixup-libcompat-Only-test-the-strerror-if-sys_errlis.patch \
 0006-dpkg-genbuildinfo-warning-unknown-CC-system-type-i68.patch \
 0007-dpkg-genbuildinfo-error-cannot-open-var-lib-dpkg-sta.patch \
-| GIT_WORK_TREE=dpkg GIT_DIR=dpkg/.git git am
+| \
+GIT_WORK_TREE=dpkg GIT_DIR=dpkg/.git git am
 
 cat \
 0001-apt-get-install-cvs2svn-cvs.patch \
 0002-mkdir-cannot-create-directory-_-usr_-Read-only-file-.patch \
 0003-Package-docx2txt.patch \
-| GIT_WORK_TREE=docx2txt GIT_DIR=docx2txt/.git git am
+| \
+GIT_WORK_TREE=docx2txt GIT_DIR=docx2txt/.git git am
 
 cat \
 0001-cygwin.patch.~2~ \
 0002-usr-bin-install-missing-destination-file-operand-aft.patch \
 0003-doc.patch.~1~ \
-| GIT_WORK_TREE=swish++ GIT_DIR=swish++/.git git am
+| \
+GIT_WORK_TREE=swish++ GIT_DIR=swish++/.git git am
+
+rm -f debconf/debconf debconf/Debconf
+GIT_WORK_TREE=debconf GIT_DIR=debconf/.git git checkout Debconf
 
 cat \
 0001-munge.patch \
-| GIT_WORK_TREE=debconf GIT_DIR=debconf/.git git am
+0002-No-candidate-position-for-the-addendum.patch \
+| \
+GIT_WORK_TREE=debconf GIT_DIR=debconf/.git git am
 
 cat \
 0001-e6b3ba4-dh_testroot-root_requirements-no-longer-read.patch \
 0002-Can-t-exec-dh_strip_nondeterminism-No-such-file-or-d.patch \
-| GIT_WORK_TREE=dh-autoreconf GIT_DIR=dh-autoreconf/.git git am
+| \
+GIT_WORK_TREE=dh-autoreconf GIT_DIR=dh-autoreconf/.git git am
 
 cat \
 0001-cygwin.patch.~3~ \
 0002-root.patch \
 0003-doc.patch \
 0004-error.patch \
-| GIT_WORK_TREE=doc-base GIT_DIR=doc-base/.git git am
+| \
+GIT_WORK_TREE=doc-base GIT_DIR=doc-base/.git git am
 
 cat \
 0001-cygwin.patch \
@@ -69,12 +87,14 @@ cat \
 0013-doxie.patch \
 0014-revert.patch \
 0015-distributor.patch \
-| GIT_WORK_TREE=dwww GIT_DIR=dwww/.git git am
+| \
+GIT_WORK_TREE=dwww GIT_DIR=dwww/.git git am
 
 cat \
 0001-usr-bin-install-cannot-stat-.-fakeroot.1-No-such-fil.patch \
 0002-i686-pc-cygwin.patch \
-| GIT_WORK_TREE=fakeroot GIT_DIR=fakeroot/.git git am
+| \
+GIT_WORK_TREE=fakeroot GIT_DIR=fakeroot/.git git am
 
 cat \
 0001-use-Config-print-Config-vendorlib.patch \
@@ -85,20 +105,26 @@ cat \
 GIT_WORK_TREE=strip-nondeterminism GIT_DIR=strip-nondeterminism/.git git am
 
 make \
+DISTRIBUTOR=doxie \
+prefix=/usr \
 	dpkg/configure dpkg \
 	install -C dpkg \
 	fakeroot/configure fakeroot \
 	tar/configure tar \
 	install -C tar \
-	debhelper \
-	install -C debhelper \
 	~/.cpan \
 	strip-nondeterminism/debian \
 	intltool-debian \
+	install -C intltool-debian \
 	publib/configure \
 	publib \
+	install -C publib \
 	doc-base \
+	install -C debhelper \
 	debconf \
+	install -C debconf \
 	swish++ \
+	install -C swish++ \
 	dwww \
+	install -C dwww \
 	/var/cache/debconf /var/lib/doc-base/documents po-debconf \
