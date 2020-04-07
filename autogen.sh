2035c4abf79ec643825d8c79cbb6ae72526ec9f0
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2020-04-07 16:29:40 +0200

    dpkg

--- autogen.sh
+++ autogen.sh
@@ -29,7 +29,6 @@ GIT_WORK_TREE=calm GIT_DIR=calm/.git git am
 cat \
 0001-fixup-Release-1.19.7.patch \
 0002-error-cannot-get-project-version.patch \
-0003-dpkg-architecture.pl-error-dpkg-print-architecture-f.patch \
 0004-error-Unknown-architecture-cannot-build-start-stop-d.patch \
 0005-fixup-libcompat-Only-test-the-strerror-if-sys_errlis.patch \
 0006-dpkg-genbuildinfo-warning-unknown-CC-system-type-i68.patch \
