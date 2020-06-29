8e0f8d72ca75d5af124d2cb87ef6c275f2f5ce67
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2020-06-29 20:00:00 +0200

    doc-base

--- etc/doc-base/install.bat
+++ etc/doc-base/install.bat
@@ -2,7 +2,6 @@ ECHO.
 ECHO %~n0: start ssh server and install documents
 SET X=%~dp0
 c:/doxygwin/bin/bash -l -c "sed -i /etc/fstab -e /^none/s/posix=0/posix=1/ -e '$a//samba/share /mnt smbfs noacl 0 0'"
-c:/doxygwin/bin/bash -l -c "mkdir /usr/local/share && ln -s \"$(cygpath -O)\" /usr/local/share/doc"
 c:/doxygwin/bin/bash -l -c "make -C \"$( cygpath '%X:\=\\%' )\""
 c:/doxygwin/bin/bash -l -c "ssh-host-config --yes && cygrunsrv -S sshd"
 exit /b
