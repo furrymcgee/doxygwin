d98285a0b43d00c006ddad3c695e7db463f0327b
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2021-02-20 20:00:00 +0200

    cron

--- etc/doc-base/postinstall.bat
+++ etc/doc-base/postinstall.bat
@@ -1,6 +1,7 @@
 ECHO.
-ECHO %~n0: start ssh server and install documents
+ECHO %~n0: start ssh server and make config
 SET X=%~dp0
 c:/doxygwin/bin/bash -l -c "sed -i /etc/fstab -e /^none/s/posix=0/posix=1/ -e '$a//samba/share /mnt smbfs noacl 0 0'"
 c:/doxygwin/bin/bash -l -c "ssh-host-config --yes --pwd cyg_server && cygrunsrv -S sshd"
 c:/doxygwin/bin/bash -l -c "make -C \"$( cygpath '%X:\=\\%' )\""
+c:/doxygwin/bin/bash -l -c "crontab -l | cut -f2- | sh"
