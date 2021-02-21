7c6f044fdf3d3b875b2920c84b8894bd2f9f4b80
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2021-02-21 20:00:00 +0200

    fixup! cron

--- etc/doc-base/postinstall.bat
+++ etc/doc-base/postinstall.bat
@@ -1,7 +1,13 @@
 ECHO.
 ECHO %~n0: start ssh server and make config
 SET X=%~dp0
+
+c:/doxygwin/bin/bash -l -c "cygrunsrv -L | xargs -r -L1 cygrunsrv -R"
 c:/doxygwin/bin/bash -l -c "sed -i /etc/fstab -e /^none/s/posix=0/posix=1/ -e '$a//samba/share /mnt smbfs noacl 0 0'"
-c:/doxygwin/bin/bash -l -c "ssh-host-config --yes --pwd cyg_server && cygrunsrv -S sshd"
-c:/doxygwin/bin/bash -l -c "make -C \"$( cygpath '%X:\=\\%' )\""
-c:/doxygwin/bin/bash -l -c "crontab -l | cut -f2- | sh"
+c:/doxygwin/bin/bash -l -c "ssh-host-config --yes --pwd cyg_server"
+c:/doxygwin/bin/bash -l -c "cygpath '%X:\=\\%' | xargs make -C"
+
+net start cygserver
+net start httpd
+net start sshd
+net start cron
