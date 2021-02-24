dcc5da95eedebe1b9dbe8c3fb31170ac60f9a123
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2021-02-24 20:00:00 +0200

    setup

--- etc/doc-base/postinstall.bat
+++ etc/doc-base/postinstall.bat
@@ -1,11 +1,11 @@
-ECHO.
+@ECHO OFF
 ECHO %~n0: start ssh server and make config
 SET X=%~dp0
 
-c:/doxygwin/bin/bash -l -c "cygrunsrv -L | xargs -r -L1 cygrunsrv -R"
+c:/doxygwin/bin/bash -l -c "cygrunsrv -L | xargs -t -r -L1 cygrunsrv -R"
 c:/doxygwin/bin/bash -l -c "sed -i /etc/fstab -e /^none/s/posix=0/posix=1/ -e '$a//samba/share /mnt smbfs noacl 0 0'"
 c:/doxygwin/bin/bash -l -c "ssh-host-config --yes --pwd cyg_server"
-c:/doxygwin/bin/bash -l -c "cygpath '%X:\=\\%' | xargs make -C"
+c:/doxygwin/bin/bash -l -c "cygpath '%X:\=\\%' | xargs -t make -C"
 
 net start cygserver
 net start httpd
