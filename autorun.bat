89cae0f66cd9f6424195d3af4ce002360fc65260
furrymcgee <furrymcgee@lippydanger.jumpingcrab.com>
2021-04-05 20:00:00 +0200

    3

--- autorun.bat
+++ autorun.bat
@@ -1,7 +1,7 @@
 REM install doygwin packages and configuration
 
 REM install cygwin with all packages 
-call %~dp0/cygwin-auto-install/cygwin-install
+call %~dp0\cygwin-auto-install\cygwin-install
 
 REM configure dwww and doc-base documents
-call %~dp0/etc/doc-base/postinstall
+call %~dp0\etc\doc-base\postinstall
